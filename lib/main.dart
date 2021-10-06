81b0bf8a91213b9effa6d1e8a33b859cc7a3d8af
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-10-06 14:34:20 -0500

    Añadimos la libreria material

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return const MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
-    );
-  }
+  runApp();
 }
