7a1313d26862c354f878bf0797329215f325ce0d
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-10-06 14:48:23 -0500

    Añadimos el correcciones ofrecidas por flutter para una mejor extructura

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(context) {
     // ignore: prefer_const_constructors
